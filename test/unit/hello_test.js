b317c8f9804a11b220030e4731e1792b47d79d42
Meshaal Mohammed <mmdamigh@gmail.com>
2024-01-24 11:29:23 +0300

    Update hello_test.js

--- test/unit/hello_test.js
+++ test/unit/hello_test.js
@@ -6,11 +6,11 @@ const Lab   = require('@hapi/lab');
 const lab   = exports.lab = Lab.script();
 const Hello = require('../../lib/hello.js');
 
-lab.experiment('test greetings', () => {
+// lab.experiment('test greetings', () => {
 
-    lab.test('greets with name ', () => {
+//     lab.test('greets with name ', () => {
 
-        Code.expect(Hello('Homer')).to.equal('Hello, Homer!');
-    });
+//         Code.expect(Hello('Homer')).to.equal('Hello, Homer!');
+//     });
 
-});
+// });
